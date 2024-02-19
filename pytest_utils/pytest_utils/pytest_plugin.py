f1017563e785db099e1b7f6e8dd1c08aa4f79c95
erlebach <gordon.erlebach@gmail.com>
2024-02-19 18:09:44 -0500

    Test 4 initial tests

--- pytest_utils/pytest_utils/pytest_plugin.py
+++ pytest_utils/pytest_utils/pytest_plugin.py
@@ -7,6 +7,9 @@ def pytest_runtest_makereport(item):
     x = yield
     x._result.max_score = getattr(item._obj, 'max_score', 0)
     x._result.visibility = getattr(item._obj, 'visibility', 'visible')
+    x._result.partial_score = getattr(item._obj, 'partial_score', '0')
+    x._result.explanation = getattr(item._obj, 'explanation', None)
+    x._result.hide_errors = getattr(item._obj, 'hide_errors', None)
 
 def pytest_terminal_summary(terminalreporter, exitstatus):
     json_results = {
@@ -16,6 +19,9 @@ def pytest_terminal_summary(terminalreporter, exitstatus):
         'test_name_format': 'text',
         'stdout_visibility': 'hidden',
         'visibility': 'visible',
+        'explanation': '',
+        'hide_errors': '',
+        'partial_score': 0,
         'extra_data': {},
         'tests': []
     }
@@ -24,10 +30,10 @@ def pytest_terminal_summary(terminalreporter, exitstatus):
     total_obtained_score = 0
     total_max_score = 0
 
-    if ('passed' in terminalreporter.stats):
-        all_tests = all_tests + terminalreporter.stats['passed']
     if ('failed' in terminalreporter.stats):
         all_tests = all_tests + terminalreporter.stats['failed']
+    if ('passed' in terminalreporter.stats):
+        all_tests = all_tests + terminalreporter.stats['passed']
 
     # First, calculate total scores
     for s in all_tests:
@@ -36,16 +42,40 @@ def pytest_terminal_summary(terminalreporter, exitstatus):
             total_obtained_score += s.max_score
 
     for s in all_tests:
-        output = ''
+        # Printed after each message
+        output = "" #'Nothing to output (debugging)'
         rescaled_score = 0
         score = s.max_score
+
+        """
+        # Not a good idea to provide links to the test code, because I do not 
+        # wish to students to see it. 
+        # Construct a clickable link using the file path and line number
+        file_path = s.location[0]  # The file path of the test
+        line_number = s.location[1]  # The line number of the test
+        clickable_link = f"{file_path}:{line_number}"
+        # Include the clickable link in the output
+        output += f"\nClick here to view the test: {clickable_link}"
+        """
+        #output += f"\nExplanation: {s.explanation!s}"  # str()
+
         if (s.outcome == 'failed'):
-            error_message = str(s.longrepr.reprcrash.message)
-            output = error_message
+
+            if s.explanation is not None:
+                # print adittional explanation
+                output += f"\nExplanation: {s.explanation!s}"  # str()
+
+            if hasattr(s, 'hide_errors') and s.hide_errors is not None:
+                output += s.hide_errors
+            else:
+                # print assert error message
+                error_message = str(s.longrepr.reprcrash.message)
+                output += error_message
+
             score = 0
             status = 'failed'  # not sure if needed
         elif (s.outcome == 'passed'):
-            rescaled_score = (s.max_score / total_max_score) * 100
+            rescaled_score = (s.max_score / (total_max_score+.01)) * 100
             status = 'passed'
         else:
             output = ''
@@ -54,14 +84,17 @@ def pytest_terminal_summary(terminalreporter, exitstatus):
         json_results["tests"].append(
             {
                 'score': rescaled_score,
+                'partial_score': s.partial_score,
                 'non-scaled_score': score,
                 'max_score': s.max_score,
                 'name': s.location[2],
                 'output': output,
                 'visibility': s.visibility,
+                'explanation': s.explanation,
                 'status': status
             }
         )
 
     with open('results.json', 'w') as results:
+        #print(json_results)
         results.write(json.dumps(json_results, indent=4))
--- run_tests.x
+++ run_tests.x
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Runs on Gradescope server
-export PYTHONPATH=/autograder/MAKE-STUDENT-OUTPUT/CODE:.:pytest_utils:src_with_answers
+# CODE: student code
 
-#due_date="2024-02-15"
-due_date=""  # fill in
+# student_code_with_answers is on my local system
+export PYTHONPATH=student_code_with_answers:/autograder/MAKE-STUDENT-OUTPUT/CODE:.:pytest_utils:instructor_code_with_answers
+
+due_date="2024-02-20"
 
 # Current date in YYYY-MM-DD format
 current_date=$(date '+%Y-%m-%d')
@@ -14,13 +16,20 @@ echo "due_date" : $due_date
 if [[ "$current_date" < "$due_date" ]]; then
     echo "Current date is earlier than the due date."
 
-    pytest -s tests/test_structure.py
+#pytest -s tests/test_structure.py 
+pytest -s tests/test_structure.py tests/test_answers.py
+#pytest -s tests/test_answers.py
+
+#pytest -s tests/test_structure.py::test_structure_question3_a_SSE_eval_float_lparen_rparen
+#pytest -s tests/test_answers.py::test_answers_question7_a_string
+#pytest -s tests/test_answers.py::test_answers_question3_a_SSE_eval_float
+
 
 elif [[ "$current_date" > "$due_date" ]]; then
     echo "Current date is later than the due date."
 
-    pytest -s tests/test_structure.py
-    pytest -s tests/test_answers.py
+#pytest -s tests/test_structure.py
+#pytest -s tests/test_answers.py
 else
     echo "Current date is the due date."
 fi
